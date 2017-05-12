8ec7dd4c58d9f7fbf396bd2398a9f38c31fcb844
Philip Crockett <philipmayoo@gmail.com>
2017-05-12 19:49:40 +0200

    Initial TCP server and test client for the XMPP protocol

--- src/senaka_sup.erl
+++ src/senaka_sup.erl
@@ -27,9 +27,20 @@ start_link() ->
 %%====================================================================
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
-init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+init(_Args) ->
+    {ok, Port} = application:get_env(senaka, port),
+    Flags = #{strategy => one_for_one, intensity => 1, period => 5},
+    Children = [child(tcp_server, worker, [Port], [])],
+    {ok, {Flags, Children}}.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+child(Module, Type, Args, Modules) ->
+    #{id => Module,
+      start => {Module, start_link, Args},
+      restart => permanent,
+      shutdown => 5000,
+      type => Type,
+      modules => Modules}.
