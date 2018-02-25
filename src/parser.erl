02e3419c5c7da3d24a97864ee4a4d4928a29fa0d
Philip Crockett <philipmayoo@gmail.com>
2018-02-25 11:23:56 +0100

    Working on adding XML parsing

--- src/parser.erl
+++ src/parser.erl
@@ -1,4 +1,11 @@
 -module(parser).
 
+-export(parse_xml/1, write_xml/2).
 
+write_xml(Data, "stream") ->
+    io:format("Writing stream");
+write_xml(Data, "message") ->
+    io:format("Writing message").
 
+parse_xml(String) ->
+    xmerl_scan:string(String).
